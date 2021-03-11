e1d4d3a8a52c8dfb68d59236300fc007c007e7df
mu <mumayank@gmail.com>
2021-03-11 20:46:21 +0530

    add db related files

---
+++ app/src/main/java/com/mumayank/howstheweather/db/Db.kt
@@ -0,0 +1,19 @@
+package com.mumayank.howstheweather.db
+
+import android.app.Application
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+object Db {
+    private lateinit var db: AppDatabase
+
+    fun getDb(applicationContext: Application): AppDatabase {
+        if (this::db.isInitialized.not()) {
+            db = Room.databaseBuilder(
+                applicationContext,
+                AppDatabase::class.java, "database-name"
+            ).build()
+        }
+        return db
+    }
+}
