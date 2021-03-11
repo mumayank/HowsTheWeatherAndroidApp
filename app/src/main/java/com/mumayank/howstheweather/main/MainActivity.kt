3f240bd85e9bfe79ae7d0f390a0c4a553fa8b658
mu <mumayank@gmail.com>
2021-03-11 20:48:38 +0530

    update main activity

--- app/src/main/java/com/mumayank/howstheweather/main/MainActivity.kt
+++ app/src/main/java/com/mumayank/howstheweather/main/MainActivity.kt
@@ -1,20 +1,26 @@
-package com.mumayank.howstheweather
+package com.mumayank.howstheweather.main
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.GravityCompat
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
+import com.mumayank.howstheweather.help.HelpActivity
+import com.mumayank.howstheweather.R
+import com.mumayank.howstheweather.settings.SettingsActivity
 import com.mumayank.howstheweather.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,11 +29,20 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbar)
         setupNavigationIfApplicable()
         setupBottomNavigationIfApplicable()
+        if (viewModel.singleDayForecastResponse.value == null) {
+            viewModel.getData()
+        }
+        viewModel.singleDayForecastResponse.observe(this) {
+            Log.e("", "")
+        }
     }
 
     private fun setupNavigationIfApplicable() {
         if (binding.drawerLayout != null) {
-            val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar, R.string.app_name, R.string.app_name)
+            val toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.toolbar,
+                R.string.app_name,
+                R.string.app_name
+            )
             binding.drawerLayout!!.addDrawerListener(toggle)
             toggle.syncState()
             val navHostFragment =
